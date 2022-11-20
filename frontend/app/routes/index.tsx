b85f427a8ad2a162ffe2bb694bc72285503566da
Wilson <fcsie55@gmail.com>
2022-11-20 21:26:21 +0800

    chore: added session cookie options

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,10 +1,5 @@
-export default function Index() {
-  return (
-    <div className="">
-      <h1 className="">Welcome to Remix</h1>
-      <ul>
-        <li className="text-3xl">Hello world hello</li>
-      </ul>
-    </div>
-  );
-}
+import type { LoaderArgs, LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
+  return;
+};
