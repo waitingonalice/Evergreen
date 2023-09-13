8b3ee429541175a20f665262d298047b01c093f4
Wilson <fcsie55@gmail.com>
2023-09-13 23:48:05 +0800

    feat: add middleware and integrate pg client deps

---
+++ backend/src/utils/pg.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+
+/* eslint-disable lines-between-class-members */
+import { Pool } from "pg";
+
+interface QueryConfigType {
+  text: string;
+  values?: any[];
+  name?: string;
+  rowMode?: string;
+}
+class PgClient {
+  private client?: Pool;
+  private lastQuery?: string;
+
+  constructor() {
+    const config = {
+      connectionString: process.env.DATABASE_URL,
+      allowExitOnIdle: false,
+      connectionTimeoutMillis: 20000, // 20 seconds
+      idle_in_transaction_session_timeout: 20000, // 20 seconds
+    };
+
+    const pool = new Pool(config);
+    this.client = pool;
+
+    pool.on("error", (err) => {
+      console.error("Unexpected error on idle client", err);
+      pool.end();
+      process.exit(-1);
+    });
+  }
+
+  clientHealthCheck = async () => {
+    try {
+      await this.client?.query("SELECT NOW()");
+      const totalCount = this.client?.totalCount;
+      const idleCount = this.client?.idleCount;
+      const waitingCount = this.client?.waitingCount;
+      console.log("totalCount", totalCount);
+      console.log("idleCount", idleCount);
+      console.log("waitingCount", waitingCount);
+      console.log("lastQuery", this.lastQuery);
+    } catch (err) {
+      console.error("PostgreSQL is not connected");
+      console.error(err);
+    }
+  };
+
+  query = async <T>({ text, values, name }: QueryConfigType) => {
+    try {
+      this.lastQuery = text;
+      const res = await this.client?.query({
+        text,
+        values,
+        name,
+      });
+      return res as T;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+}
+
+export const pg = new PgClient();
