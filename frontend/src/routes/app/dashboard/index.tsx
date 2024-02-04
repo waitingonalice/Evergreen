89c672dbcb0b3d92133745954f3f07b7b594a2a4
Wilson <fcsie55@gmail.com>
2024-02-04 17:07:31 +0800

    chore: remove console log

--- frontend/src/routes/app/dashboard/index.tsx
+++ frontend/src/routes/app/dashboard/index.tsx
@@ -1,15 +1,12 @@
 import { Main } from "~/components";
 
-const Dashboard = () => {
-  console.log(window.location);
-  return (
-    <Main>
-      <Main.Header />
-      <Main.Content>
-        <h1>Dashboard</h1>
-      </Main.Content>
-    </Main>
-  );
-};
+const Dashboard = () => (
+  <Main>
+    <Main.Header />
+    <Main.Content>
+      <h1>Dashboard</h1>
+    </Main.Content>
+  </Main>
+);
 
 export default Dashboard;
